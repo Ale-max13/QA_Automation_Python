9c4663a26b86452c1015e3a8f2f543a84477fb52
Ale-max13 <kitiale1313@gmail.com>
2025-07-10 00:36:22 +0200

    Правки по PEP8: сокращены длинные строки в lesson_3_task_3.py

--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -7,6 +7,7 @@ to_address = Address("654321", "Сочи", "Гагарина", "25", "5")
 mail = Mailing(to_address, from_address, 500, "TR123456789RU")
 
 print(
-    f"Отправление {mail.track} из {mail.from_address.index}, {mail.from_address.city}, "
-    f"{mail.from_address.street}, {mail.from_address.house} - {mail.from_address.apartment}"
+    f"Отправление {mail.track} из {mail.from_address.index}, "
+    f"{mail.from_address.city}, {mail.from_address.street}, "
+    f"{mail.from_address.house} - {mail.from_address.apartment}"
 )
