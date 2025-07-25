bef46f5a0fd7c9cada2c7e905309c52cbf955c9f
Ale-max13 <kitiale1313@gmail.com>
2025-07-26 01:58:55 +0200

    Добавлен скрипт авторизации через форму (lesson05_task4.py)

---
+++ lesson_5/lesson05_task4.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/login")
+
+username_input = driver.find_element(By.ID, "username")
+username_input.send_keys("tomsmith")
+
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys("SuperSecretPassword!")
+
+login_button = driver.find_element(By.CSS_SELECTOR, "button.radius")
+login_button.click()
+
+message = driver.find_element(By.ID, "flash")
+print(message.text.strip())
+
+time.sleep(2)
+driver.quit()
