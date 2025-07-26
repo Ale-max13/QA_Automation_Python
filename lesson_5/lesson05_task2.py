e29f2da1c734c8b2c0a14da54eb0b0e604d9bc74
Ale-max13 <kitiale1313@gmail.com>
2025-07-26 02:08:37 +0200

    PEP8: fix line too long and indentation in lesson05_task2.py

--- lesson_5/lesson05_task2.py
+++ lesson_5/lesson05_task2.py
@@ -6,7 +6,10 @@ driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/dynamicid")
 time.sleep(1)
 
-driver.find_element(By.XPATH, "//button[text()='Button with Dynamic ID']").click()
+driver.find_element(
+    By.XPATH, "//button[text()='Button with Dynamic ID']"
+).click()
+
 
 time.sleep(2)
 driver.quit()
