050b2c9eeca20ee3da6773771189407735c3747e
Ale-max13 <kitiale1313@gmail.com>
2025-08-02 00:20:51 +0200

    Успешный тест формы с Microsoft Edge  поля заполняются, валидация ZIP

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def test_form_validation():
+    service = Service(executable_path="drivers/msedgedriver.exe")
+    driver = webdriver.Edge(service=service)
+    wait = WebDriverWait(driver, 10)
+
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.CSS_SELECTOR, "input[type='email']").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone").send_keys("+798589998787")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+
+    time.sleep(15)
+    driver.quit()
