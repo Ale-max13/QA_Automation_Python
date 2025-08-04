eb124f2620e491b59f6fd452a4de929548523531
Ale-max13 <kitiale1313@gmail.com>
2025-08-04 23:12:41 +0200

    Исправлен автотест калькулятора, проверку flake8 прошёл

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,30 +1,34 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
 
-def test_calc_slow():
-    service = Service(executable_path=os.path.abspath("lesson_5/geckodriver.exe"))
-    driver = webdriver.Firefox(service=service)
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-    try:
-        delay_input = driver.find_element(By.ID, "delay")
-        delay_input.clear()
-        delay_input.send_keys("45")
+@pytest.fixture
+def driver():
+    options = Options()
+    service = Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
 
-        driver.find_element(By.XPATH, "//span[text()='7']").click()
-        driver.find_element(By.XPATH, "//span[text()='+']").click()
-        driver.find_element(By.XPATH, "//span[text()='8']").click()
-        driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-        WebDriverWait(driver, 50).until(
-            EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
-        )
-        result = driver.find_element(By.CLASS_NAME, "screen").text
-        assert result == "15"
+def test_calc_addition(driver):
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+    )
 
-    finally:
-        driver.quit()
+    delay_input = driver.find_element(By.CSS_SELECTOR, "input#delay")
+    delay_input.clear()
+    delay_input.send_keys("45")
+
+    for symbol in ["7", "+", "8", "="]:
+        driver.find_element(By.XPATH, f"//span[text()='{symbol}']").click()
+
+    result = WebDriverWait(driver, 60).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "15")
+    )
+    assert result, "Результат не равен 15 через 45 секунд"
