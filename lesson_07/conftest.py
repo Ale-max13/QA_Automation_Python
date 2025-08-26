3cacacc1b87a4e567db77dd46534129c8ec25a28
Ale-max13 <kitiale1313@gmail.com>
2025-08-27 01:14:26 +0200

    lesson_07: update conftest.py to run Chrome in incognito and disable password manager

--- lesson_07/conftest.py
+++ lesson_07/conftest.py
@@ -7,6 +7,17 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def driver():
     options = ChromeOptions()
     options.add_argument("--window-size=1280,900")
+    options.add_argument("--incognito")
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+    }
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument(
+        "--disable-features=PasswordManagerOnboarding,PasswordLeakDetection"
+    )
+
     drv = webdriver.Chrome(options=options)
     drv.set_page_load_timeout(60)
     yield drv
